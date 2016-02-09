1c7f418ec1775e5f22feb8ca0e1f5cf20cd23967
ConsultecR <ConsultecR@ConsultecR07-PC>
2016-02-08 20:05:34 -0500

    3 version

--- programa1/src/programa1/Programa1.java
+++ programa1/src/programa1/Programa1.java
@@ -5,11 +5,12 @@ public class Programa1 {
 
 
     public static void main(String[] args) {
-        int a = 5+5 , suma = 0 ;
-        suma = a;
-        System.out.println("La suma es :"+suma);
-        
+             
        
+    }
+    
+    public void metodo(){
+        System.out.println("Esto es un metodo");
     }
     
 }
